a463966264b9d51df180db00e3b4cb92061a66d0
Utkarsh's Galaxy <utkarshsharma12157@gmail.com>
2025-06-10 00:08:28 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Frontend Mentor - Product list with cart solution
+
+This is a solution to the [Product list with cart challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/product-list-with-cart-5MmqLVAp_d). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Add items to the cart and remove them
+- Increase/decrease the number of items in the cart
+- See an order confirmation modal when they click "Confirm Order"
+- Reset their selections when they click "Start New Order"
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![](./src/assets/Screenshot%20(405).png)
+![](./src/assets/Screenshot%20(406).png)
+![](./src/assets/Screenshot%20(407).png)
+
+### Links
+
+- Solution URL: [Add solution URL here]
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [Vite](https://vite.dev/) - JavaScript build tool
+- [React](https://reactjs.org/) - JS library
+- [Next.js](https://nextjs.org/) - React framework
+- [TailwindCss](https://tailwindcss.com/) - For Tailwind Css
+
+
+### What I learned
+
+Its been a week since i uploaded any project on frontend mentor, so after started to learn REACT, it was overwhelming, i want to learn more before starting to build projects, which is a common phase for me whenever i got introduced a new thing.
+
+So by watching those tutorials and actually i did completed a small project previously (result summary component) one, but this project has a lot of things, i think mostly are not that advanced, now when i completed i'm seeing it that way, but before it looks like a mess.
+
+although i took help from gemini, i layed-out the cards and i knew how to change the state for add to cart buttons and how to add a counter for increasing the quantity, but it was a week ago i knew, so with a little push it came along,
+
+but the main problem was rendering selected cards with correct quantity, that took a lot of time even with gemini, and then as it always happens, gemini provide a code which i didn't understands very well, its mostly a automatic code which works under the hood, so i can't see how is it working.(i think you know what i am trying to say).
+
+anyways, it was a good experience, that's how you learn, now if i had to apply same login in another project i will know how to do it, yes i have to open this file and then change accordingly but after some projects i'll do it on my own.
+
+### Continued development
+
+For now i need to start with frontend but i'm going to continue backend devoloping and then if everything goes correctly then will countinue either in robotics coding and developement or in gaming industry.
+
+## Author
+
+- Frontend Mentor - [@Utkarsh9571](https://www.frontendmentor.io/profile/Utkarsh9571)
---
+++ src/App.jsx
@@ -0,0 +1,250 @@
+import { useState } from "react";
+import data from './data.json';
+
+function DessertCart ({ item, cartItem, onUpdateGlobalCart }) {
+  
+  const isInCart = !!cartItem;
+  const quantity =  cartItem ? cartItem.quantity : 0;
+
+  const handleAddToCart = () => {
+    onUpdateGlobalCart(item, 1);
+  }
+
+  const handleIncreaseQuantity = () => {
+      onUpdateGlobalCart(item, 1);
+  }
+
+  const handleDecreaseQuantity = () => {
+        onUpdateGlobalCart(item, -1);
+  }
+
+  return(
+    <div key={item.id} className="relative ">
+      <img 
+        src={item.image.mobile} 
+        alt={item.category} 
+        className={`rounded-xl mt-6 ${!isInCart ? 'border-0': 'border-2 border-projectRed'}` }
+      />
+
+      <div>
+        {isInCart ? (
+          <div 
+            className="bg-projectRed flex justify-between w-1/2 p-3 rounded-full 
+            absolute bottom-16 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <button
+              onClick={handleDecreaseQuantity}
+              className="border-white border-2 rounded-full p-1 cursor-pointer"
+              disabled={quantity <= 0}
+              >
+              <img src="assets\images\icon-decrement-quantity.svg" alt="icon decrement quantity" />
+            </button>
+
+            <span className="text-white text-md">{quantity}</span>
+            
+            <button
+              onClick={handleIncreaseQuantity}
+              className="border-white border-2 rounded-full p-1 cursor-pointer"
+              >
+              <img src="assets\images\icon-increment-quantity.svg" alt="icon increment quantity" />
+            </button>
+        </div>
+      ) : (
+        <button 
+          onClick={handleAddToCart}
+          className="flex w-1/2 justify-center p-3 pl-7 pr-7 gap-2 border border-rose500 justify-self-center font-medium bg-white rounded-full 
+          absolute left-1/2 -translate-x-1/2 -translate-y-1/2 bottom-16 hover:text-projectRed cursor-pointer hover:border-projectRed md:gap-1 md:pl-5 md:pr-5" 
+          >
+          <img src="assets\images\icon-add-to-cart.svg" alt="icon add to cart" />
+          <h1 className="text-sm">Add to Cart</h1>
+        </button>
+      )}
+      </div>
+        <h2 className="text-sm font-medium text-rose500 mt-10">{item.category}</h2>
+        <h3 className="text-md font-semibold text-rose900">{item.name}</h3>
+        <h4 className= "font-semibold text-projectRed">{`${"$" + item.price.toFixed(2)}`}</h4>
+      </div>
+    )
+}
+
+
+
+export default function App() {
+  const [items] = useState(data);
+  const [cartItems, setCartItems] = useState([]);
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
+  const handleUpdateGlobalCart =(dessertItem, change) => {
+    setCartItems(prevCartItems => {
+      const existingItemIndex = prevCartItems.findIndex(cartItems => cartItems.id === dessertItem.id);
+
+      if (existingItemIndex > -1) {
+        const updatedCartItems = [...prevCartItems];
+        updatedCartItems[existingItemIndex] = {
+          ...updatedCartItems[existingItemIndex],
+          quantity: updatedCartItems[existingItemIndex].quantity + change
+        };
+        
+        if (updatedCartItems[existingItemIndex]. quantity <= 0) {
+          return updatedCartItems.filter(item => item.id !== dessertItem.id);
+        }
+        return updatedCartItems;
+      } else {
+        if (change > 0) {
+          return [...prevCartItems, { ...dessertItem, quantity: change }];
+        }
+        return prevCartItems;
+      }
+    });
+  };
+
+  const totalCartQuantity = cartItems.reduce((total, item) => total + item.quantity, 0);
+
+  const totalOrderAmount = cartItems.reduce((total, item) => total + (item.quantity * item.price), 0);
+
+  const handleConfirmOrder = () => {
+    setShowConfirmation(true);
+  }
+
+  const handleStartNewOrder = () => {
+    setShowConfirmation(false);
+    setCartItems([]);
+  }
+
+  return (
+    <section className="relative p-6 font-red-hat bg-rose50 sm:p-14 md:p-18">
+    <h1 className="text-5xl font-bold mb-6">Desserts</h1>
+
+    <div className="flex flex-col lg:grid lg:grid-cols-3 xl:grid-cols-4 lg:gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 lg:col-span-2 xl:col-span-3 gap-8">
+      {items.map((item) => (
+        <DessertCart 
+          key={item.id} 
+          item={item} 
+          cartItem={cartItems.find(cartItems => cartItems.id === item.id)}
+          onUpdateGlobalCart={handleUpdateGlobalCart}
+        />
+      ))}
+    </div>
+
+    <div className="bg-white mt-8 p-6 rounded-2xl flex-col justify-items-center
+    lg:mt-0 lg:col-span-1 xl:col-span-1">
+      <h1 className="text-projectRed text-2xl font-bold justify-self-start">
+        Your Cart ({totalCartQuantity})
+      </h1>
+
+      {cartItems.length === 0 ? (
+        <>
+          <img src="assets\images\illustration-empty-cart.svg" alt="illustration empty cart" 
+            className="mt-12 mb-6"/>
+          <h2 className="text-rose500 font-semibold text-md" >
+            Your added items will appear hear
+          </h2>
+        </>
+      ) : (
+        <div className="pt-4 w-full">
+          {cartItems.map((cartItems) => (
+            <div
+              className="flex justify-between mb-3 pb-3 border-b border-rose300" 
+              key={cartItems.id}>
+              <div className="w-full">
+                <h3 className="font-semibold text-base text-rose900">{cartItems.name}</h3>
+                <div>
+                  <span className="font-semibold text-sm text-projectRed">{cartItems.quantity}x</span>
+                  <span className="ml-5 mr-3 text-sm font-medium text-rose400">{`@ $${cartItems.price.toFixed(2)}`}</span>
+                  <span className="font-semibold text-sm text-rose500">{`$${(cartItems.quantity * cartItems.price).toFixed(2)}`}</span>
+                </div>
+              </div>
+
+              <button
+                  onClick={() => handleUpdateGlobalCart(cartItems, -cartItems.quantity)}
+                >
+                  <img 
+                    src="assets\images\icon-remove-item.svg" 
+                    alt="icon remove item" 
+                    className="w-full border-rose300 border-2 rounded-full m-0.5 p-0.5
+                    hover:border-rose900 cursor-pointer"/>
+                </button>
+
+            </div>
+          ))}
+
+            <div className="flex justify-between items-center">
+              <span className="font-medium text-rose500">Order Total</span>
+              <span className="text-2xl font-bold">{`$${cartItems.reduce((total, item) => total + (item.quantity * item.price), 0).toFixed(2)}`}</span>
+            </div>
+
+            <div className="flex justify-center gap-2 bg-rose50 p-4.5 rounded-2xl mt-6 mb-6">
+              <img src="assets\images\icon-carbon-neutral.svg" alt="icon carbon neutral" />
+              <h1 className="text-sm text-rose900">This is a <span className="font-semibold">carbon-neutral </span>delivery</h1>
+            </div>
+
+            <button 
+              onClick={handleConfirmOrder}
+              className="bg-projectRed rounded-full p-4 w-full text-lg text-white font-medium
+              hover:bg-orange-900 cursor-pointer"
+              >
+              Confirm Order
+            </button>
+        </div>
+      )}
+       </div>
+    </div>
+    
+       {showConfirmation && (
+        <div className="fixed bottom-0 right-0 bg-black/50 h-full w-full">
+          <div className="fixed bottom-0 left-0 p-7 bg-white w-full h-fit rounded-2xl 
+          md:w-fit md:min-w-md md:h-2/3 md:bottom-1/6 md:left-1/5 lg:left-1/4 xl:left-1/3 md:overflow-scroll hide-scrollbar">
+            <div>
+              <img
+                className="mb-5" 
+                src="assets\images\icon-order-confirmed.svg" 
+                alt="icon order confirmed" />
+              <h1 className="font-bold text-4xl w-1/2">Order Confirmed</h1>
+              <h2 className="font-medium mt-4 mb-4 text-rose400">We hope you enjoy your food!</h2>
+              <div>
+                <div className="bg-rose50 p-6 mb-12">
+                  {cartItems.map((cartItems) => (
+                  <div
+                    className="flex items-center justify-between pb-5 border-b border-rose100"
+                     key={cartItems.id}>  
+                      <img 
+                        className="w-1/5 rounded-md"
+                        src={cartItems.image.thumbnail} 
+                        alt="cart item thumbnail" />
+
+                      <div className="w-1/2">
+                        <p className="font-semibold text-md mb-1 text-left text-rose900">{cartItems.name}</p>
+                        <p className="text-rose400 font-medium ">
+                          <span className="text-projectRed font-semibold mr-3.5">
+                            {cartItems.quantity}x
+                          </span> 
+                          @ ${cartItems.price.toFixed(2)}
+                        </p>
+                      </div>
+
+                      <h1 className="font-semibold  text-lg text-rose900">{`$${(cartItems.quantity * cartItems.price).toFixed(2)}`}</h1>
+                    </div>
+                  ))}
+
+                  <div className="flex justify-between items-center mt-6">
+                    <span className="font-medium text-rose500">Order Total</span>
+                    <span className="text-2xl font-bold">{`$${totalOrderAmount.toFixed(2)}`}</span>
+                  </div>
+                </div>
+     
+                <button 
+                  onClick={handleStartNewOrder}
+                  className="bg-projectRed rounded-full p-4 w-full text-lg text-white font-medium
+                  hover:bg-orange-900 cursor-pointer"
+                >
+                  Start New Order
+                </button>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        )}
+    </section>
+  )
+}
